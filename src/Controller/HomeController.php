20cf80f25e3e66a5e0a583b2ee1ce168b18a09d1
Lucas Encrenaz <lucas.encrenaz3@gmail.com>
2021-02-08 10:46:43 +0100

    All before the form

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,24 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class HomeController extends AbstractController
+{
+
+    public function __construct()
+    {
+    }
+
+    function index(Request $request): Response
+    {
+        return $this->render(
+            "home/index",
+            [
+                "name"=>$request->query->get('name')
+            ]
+        );
+    }
+
+}
